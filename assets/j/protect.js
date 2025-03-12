848b824f7abffd8ff454c09190b8bf6609cf94dc
bearcattt <ewigglet8@gmail.com>
2025-03-12 09:51:51 -0400

    fix pswd protect

--- assets/j/protect.js
+++ assets/j/protect.js
@@ -1,13 +1,13 @@
-// Delete file after development
 try {
     const p = window.atob("dGhvcg==");
-    const guess = prompt("Please enter the password to continue to the exclusive math book").toLowerCase();
-
-    if (guess === p) {
-        alert("Welcome :)");
-        return;
+    const input = prompt("Please enter the password to continue to the exclusive math book").toLowerCase();
+    // todo: make this a fake educational page til login
+    if (input === p) {
+        alert("Welcome :)"); // correct :D
     } else {
-        window.location.reload() // now they cant get through
+        alert("Incorrect password. Try again.");
+        window.location.reload(); // prevent them from seeing 
+
     }
 } catch (e) {
     alert("Error: " + e);
