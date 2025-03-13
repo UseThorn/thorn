e68bf6e4d60e3fa10e2aaaafee58b2e4b63304a3
bearcattt <ewigglet8@gmail.com>
2025-03-13 13:58:29 -0400

    fix title

--- assets/j/protect.js
+++ assets/j/protect.js
@@ -1,9 +1,9 @@
-const PASSWORD = window.atob("Z2F5");
-const STORAGE_KEY = "@thorn_math/loggedIn";
+const PASSWORD = window.atob("cHJlbWl1bTE5");
+const STORAGE_KEY = "@vortex_math/loggedIn";
 const isLoggedIn = localStorage.getItem(STORAGE_KEY) === "true";
 
 if (!isLoggedIn) {
-    document.title = "Thorn Math"
+    document.title = "Vortex Math"
     const premium = document.createElement("iframe");
     premium.style = "position: fixed; top: 0; left: 0; width: 100%; height: 100%; border: none; z-index: 100;";
     premium.src = "/math.html";
