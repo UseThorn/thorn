0ed0dc5be0a6d9b792b519391bd9796951d91d92
bearcattt <ewigglet8@gmail.com>
2025-03-17 11:50:37 -0400

    Update container.js

--- assets/j/container.js
+++ assets/j/container.js
@@ -1,4 +1,7 @@
-//TODO: game fetch code 
+window
 .fetch("./assets/request/math.json").then((response) => {
-   
+   if (!response.ok) {
+    throw new Error("response failed.")
+   }
+   //TODO: make it load games
 });
