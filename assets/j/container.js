fe7cb4aa9044ad473580fef6ca30007025f054dc
bearcattt <ewigglet8@gmail.com>
2025-03-20 23:07:46 -0400

    fixes

--- assets/j/container.js
+++
@@ -1,7 +0,0 @@
-window
-.fetch("./assets/json/gl.json").then((response) => {
-   if (!response.ok) {
-    throw new Error("response failed.")
-   }
-   //TODO: make it load games
-});
--- assets/j/protect.js
+++ assets/j/protect.js
@@ -1,14 +1,18 @@
 const PASSWORD = window.atob("cHJlbWl1bTE5");
-    document.title = "Vortex Math"
-    const link = document.createElement('link');
-    link.rel = 'icon';
-    document.head.appendChild(link);
-    link.href = '';
-    const premium = document.createElement("iframe");
-    premium.style = "position: fixed; top: 0; left: 0; width: 100%; height: 100%; border: none; z-index: 100;";
-    premium.src = "/math.html";
-    document.body.appendChild(premium);
 
+document.title = "Vortex Math";
+
+const link = document.createElement('link');
+link.rel = 'icon';
+document.head.appendChild(link);
+link.href = '';
+
+const premium = document.createElement("iframe");
+premium.style = "position: fixed; top: 0; left: 0; width: 100%; height: 100%; border: none; z-index: 100; background: white;";
+premium.src = "/math.html";
+document.body.appendChild(premium);
+
+requestAnimationFrame(() => {
     setTimeout(() => {
         let input;
         try {
@@ -18,13 +22,12 @@ const PASSWORD = window.atob("cHJlbWl1bTE5");
         }
 
         if (input === PASSWORD) {
-         //   localStorage.setItem(STORAGE_KEY, "true");
             premium.remove();
-            document.title = "Dashboard | RapidIdentity"
-                link.href = '/favicon.ico';
+            document.title = "Dashboard | RapidIdentity";
+            link.href = '/favicon.ico';
             alert("Access granted. Welcome! :)");
         } else {
             alert("Incorrect password. Access denied to nginx server.");
         }
-    }, 500);
-
+    }, 0);
+});
